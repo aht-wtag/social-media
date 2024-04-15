a74c33d1901cff9236af9a0721392b5a41c188cb
aht-wtag <tahsin.ahmed@welldev.io>
2024-04-15 19:04:46 +0600

    user model added with devise authentication gem

---
+++ db/migrate/20240415130013_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.text :bio
+      t.string :location
+
+      t.timestamps
+    end
+    add_index :users, :username, unique: true
+  end
+end
