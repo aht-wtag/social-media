706da1c30733b56d55dc9524ffe405e58dcd11bc
Tahsin_TSE_aht-wtag <163710890+aht-wtag@users.noreply.github.com>
2024-04-16 16:03:02 +0600

    User model created, password secured with bcrypt

--- db/migrate/20240416094601_create_users.rb
+++ db/migrate/20240416094601_create_users.rb
@@ -2,11 +2,14 @@ class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.string :username
+      t.string :email
+      t.string :password
       t.text :bio
-      t.string :location
+      t.text :location
 
       t.timestamps
     end
     add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
   end
 end
