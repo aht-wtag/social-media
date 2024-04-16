43c6f471e197eac2b5694037c0d4d118e8b6154d
Tahsin_TSE_aht-wtag <163710890+aht-wtag@users.noreply.github.com>
2024-04-16 16:08:55 +0600

    added validation of bio and location with max text length

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
     validates :username, presence: true
     validates :email, presence: true
     validates :password, presence: true
+    validates :bio, length: {maximum: 150}
+    validates :location, length: {maximum: 50}
 end
