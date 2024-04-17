4ff993d4a8e13a9487fc302ca6583faeb35e3cfa
Tahsin_TSE_aht-wtag <163710890+aht-wtag@users.noreply.github.com>
2024-04-17 11:29:56 +0600

    added validation test for all mandatory fields

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,24 @@
 require "rails_helper"
 RSpec.describe User,type: :model do
-    user = User.new()
-  describe "validates username" do
-    it "validates presence of username" do
+  user = User.new()
+  describe "validates username, password and email" do
+    it "validates presence of username, password, email" do
         expect(user).not_to be_valid
+    end
+
+    it "validates presence of password, email" do
+      user.username="tahsin"
+      expect(user).not_to be_valid
+    end
+
+    it "validates presence of password" do
+      user.email = "tahsin@gmail.com"
+      expect(user).not_to be_valid
+    end
+
+    it "Should be good now so that all three must ones are present" do
+      user.password = "hello world"
+      expect(user).to be_valid
     end
   end
 end
