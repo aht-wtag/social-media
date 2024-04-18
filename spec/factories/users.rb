45d1fdbaa84ca06b91971ef9b967a58c4b4bcafd
Tahsin_TSE_aht-wtag <163710890+aht-wtag@users.noreply.github.com>
2024-04-18 18:31:17 +0600

    changed from Faker to FFaker

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
     factory :user do
-      username { Faker::Lorem.characters(number: rand(1..30)) }
-      email { Faker::Lorem.characters(number: rand(1..20)) + '@gmail.com' }
+      username { FFaker::Lorem.characters(number: rand(1..30)) }
+      email { FFaker::Lorem.characters(number: rand(1..20)) + '@gmail.com' }
       password { BCrypt::Password.create('password123') }
     end
 end
