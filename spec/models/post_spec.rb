7a4f69adc2f3e856514eb45fc5d3713d2c56cee1
Tahsin_TSE_aht-wtag <163710890+aht-wtag@users.noreply.github.com>
2024-04-21 19:30:44 +0600

    post belongs to user testing added

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe Post, type: :model do
         it { is_expected.to validate_presence_of(:title) }
         it { is_expected.to validate_presence_of(:body) }
     end
+
+    describe "belongs to association" do
+        subject { FactoryBot.create(:post) }
+        it { is_expected.to belong_to(:user) }
+    end
 end
