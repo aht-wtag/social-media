b7dbf8484e0bc4337fc4aabe893bad2ad0c8eaa0
Tahsin_TSE_aht-wtag <163710890+aht-wtag@users.noreply.github.com>
2024-04-21 11:02:40 +0600

    email field changed to citext with down method so that it is reversible

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_16_094601) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_21_044950) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "citext"
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
+    t.citext "email"
     t.string "password_digest"
     t.text "bio"
     t.text "location"
