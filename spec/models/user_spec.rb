4021f30b4ad723b4c5d40c1f270e0c68584314a5
Tahsin_TSE_aht-wtag <163710890+aht-wtag@users.noreply.github.com>
2024-04-21 12:47:06 +0600

    added shoulda matchers in user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,23 +5,28 @@ RSpec.describe User, type: :model do
   user = User.create()
   
   describe "validates username, password and email" do
-    it "validates presence of username, password, email" do
-      expect(user).not_to be_valid
+    it "validates presence of username" do
+      should validate_presence_of(:username)
+      should validate_presence_of(:email)
+      should validate_presence_of(:password)
     end
 
     it "validates presence of password, email" do
       user.username = "tahsin"
-      expect(user).not_to be_valid
+      should validate_presence_of(:email)
+      should validate_presence_of(:password)
     end
 
     it "validates presence of password" do
       user.email = "tahsin@gmail.com"
-      expect(user).not_to be_valid
+      should validate_presence_of(:password)
     end
 
     it "Should be good now so that all three must ones are present" do
       user.password = "hello world"
-      expect(user).to be_valid
+      should validate_presence_of(:username)
+      should validate_presence_of(:email)
+      should validate_presence_of(:password)
     end
   end
 
