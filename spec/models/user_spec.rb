5e1c44250b64c2a467628544ff3cfb375d7c5d4b
Tahsin_TSE_aht-wtag <163710890+aht-wtag@users.noreply.github.com>
2024-04-21 19:37:53 +0600

    user has many posts association tested

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,5 +9,6 @@ RSpec.describe User, type: :model do
     subject { FactoryBot.create(:user) }
     it { is_expected.to validate_uniqueness_of(:username).case_insensitive }
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    it { is_expected.to have_many(:posts) }
   end
 end
