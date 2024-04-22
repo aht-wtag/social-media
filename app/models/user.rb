ba5da8fd1e063f6ce6be8df3ebe292553518ad3f
Tahsin_TSE_aht-wtag <163710890+aht-wtag@users.noreply.github.com>
2024-04-22 18:00:04 +0600

    related assosication of user and friendship model added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :posts, dependent: :destroy
+    has_many :friendships, dependent: :destroy
+    has_many :friends, through: :friendships
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true
