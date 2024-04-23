e3d75d03954c2c68149105cbd365dc4612072751
Tahsin_TSE_aht-wtag <163710890+aht-wtag@users.noreply.github.com>
2024-04-23 15:50:05 +0600

    email and username changed to citext properly and migrated

--- db/migrate/20240416094601_create_users.rb
+++ db/migrate/20240416094601_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :email
+      t.citext :username
+      t.citext :email
       t.string :password_digest
       t.text :bio
       t.text :location
