860de1e4573b96d117027ae2a135f1e92b4d9373
Tahsin_TSE_aht-wtag <163710890+aht-wtag@users.noreply.github.com>
2024-04-23 13:51:51 +0600

    friendship factory re configured correctly

--- spec/factories/friendships.rb
+++ spec/factories/friendships.rb
@@ -2,7 +2,5 @@ FactoryBot.define do
     factory :friendship do
         association :user
         association :friend, factory: :user
-        friend_id { FactoryBot.create(:user).id }
-        user_id { FactoryBot.create(:user).id }
     end
 end
