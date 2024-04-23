4ce27cdedefdaa8572aad46253d14f6450043948
Tahsin_TSE_aht-wtag <163710890+aht-wtag@users.noreply.github.com>
2024-04-23 19:12:01 +0600

    email regex formatting added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     validates :username, presence: true, uniqueness: true
-    validates :email, presence: true, uniqueness: true
+    validates :email, format: { with: /\A(.+)@(.+)\z/, message: "Email invalid"  },
+    presence: true, uniqueness: true
     validates :password, presence: true
 end
