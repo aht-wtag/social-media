5f8762de9d52c3ab6783532c51e3dc7e7a0711b7
Tahsin_TSE_aht-wtag <163710890+aht-wtag@users.noreply.github.com>
2024-04-23 12:19:41 +0600

    added factory bot generated fake friendship model

--- spec/factories/friendships.rb
+++ spec/factories/friendships.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
     factory :friendship do
         association :user
         association :friend, factory: :user
+        friend_id { FactoryBot.create(:user).id }
+        user_id { FactoryBot.create(:user).id }
     end
 end
