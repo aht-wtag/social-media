9e59a4effce6e731858a3c6e1c9f314d82311a28
Tahsin_TSE_aht-wtag <163710890+aht-wtag@users.noreply.github.com>
2024-04-24 17:08:25 +0600

    post table recreated with correction

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_24_052340) do
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.text "title"
-    t.text "body"
+    t.text "title", null: false
+    t.text "body", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
